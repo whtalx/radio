114355252194a2ce8ff291dd3b1fcee9be033c0d
whtalx <whtalx@gmail.com>
2019-12-25 18:16:25 +0300

    spectrum visualizer peaks

--- src/App/components/Visualization.js
+++ src/App/components/Visualization.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
+import visualize from '../functions/visualize'
+
+const bands = [2,3,4,5,6,7,10,15,21,30,42,60,84,116,167,237,334,464,696]
 
 const Canvas = styled.canvas`
   position: fixed;
@@ -9,90 +12,61 @@ const Canvas = styled.canvas`
   height: 19px;
 `
 
-const bands = [2,3,4,5,6,7,10,15,21,30,42,60,84,116,167,237,334,464,696]
-
 export default ({
-  paused,
-  analyser,
-  setVisualization,
+  state,
+  bandsBinCount,
+  peaksBinCount,
+  bandsFrequencyData,
+  peaksFrequencyData,
 }) => {
-  const spectrum = useRef(null)
-
-  const visualize = () => {
-    const canvas = spectrum.current
-    const ctx = canvas.getContext(`2d`)
-    const { width, height } = canvas
-
-    const clear = () => {
-      ctx.fillStyle = `hsl(0, 0%, 0%)`
-      ctx.fillRect(0, 0, width, height);
-
-      [...Array(19).keys()].forEach((y) => {
-        [...Array(79).keys()].forEach((x) => {
-          if (x % 2 === 0 && y % 2 === 0) {
-            ctx.fillStyle = x === 0
-              ? y % 4 === 0
-                ? `hsl(216, 60%, 81%)`
-                : `hsl(205, 100%, 71%)`
-              : y === 18
-                ? x % 4 === 0
-                  ? `hsl(216, 60%, 81%)`
-                  : `hsl(205, 100%, 71%)`
-                : `hsl(240, 41%, 16%)`
-
-            ctx.fillRect(x * width / 79, y * height / 19, width / 79, height / 19)
-          }
-        })
-      })
-    }
-
-    clear()
-
-    if (!paused) return
-
-    const gradient = ctx.createLinearGradient(0, 0, 0, height / 19 * 16)
-
-    gradient.addColorStop(0, `hsl(0, 100%, 50%)`)
-    gradient.addColorStop(.25, `hsl(30, 100%, 50%)`)
-    gradient.addColorStop(.5, `hsl(60, 100%, 50%)`)
-    gradient.addColorStop(.75, `hsl(90, 100%, 50%)`)
-    gradient.addColorStop(1, `hsl(120, 100%, 50%)`)
-
-    const drawBar = (x, w, amp) => {
-      ctx.strokeStyle = gradient//`hsl(${ 100 - 100 / 16 * amp }, 100%, 50%)`
-      ctx.lineWidth = w
-      ctx.beginPath()
-      ctx.moveTo(x + (width / 39.5), height - height / 19 * 2)
-      ctx.lineTo(x + (width / 39.5), height / 19 * (16 - amp))
-      ctx.stroke()
-    }
-
-    const bufferLength = analyser.frequencyBinCount
-    const dataArray = new Uint8Array(bufferLength)
-    analyser.getByteFrequencyData(dataArray)
-
-    bands.forEach((band, i) => {
-      const x = i === 0
-        ? 2 * width / 79
-        : (((i - 1) * 4) + 6) * width / 79
-      const amp = Math.floor(16 / 255 * dataArray[band])
-
-      drawBar(x, width / 79 * 3, amp)
-    })
-
-    requestAnimationFrame(visualize)
-  }
+  const canvasRef = useRef(null)
+  const [controller, setController] = useState(null)
 
   useEffect(
     () => {
-      if (!spectrum.current) return
-      setVisualization(visualize)
-      visualize()
-    },// eslint-disable-next-line
-    []
+      if (state === `playing`) {
+        console.log(`creating controller`)
+        setController(new AbortController())
+      } else {
+        controller && controller.abort()
+      }
+    }, // eslint-disable-next-line
+    [state]
+  )
+
+  useEffect(
+    () => {
+      if (!controller) return
+
+      const signal = controller.signal
+      signal.addEventListener(`abort`, () => {
+        console.log(`destroying controller`)
+        setController(null)
+      })
+
+      const sendBands = () => {
+        if (signal.aborted) return
+
+        const bandsData = new Uint8Array(bandsBinCount)
+        bandsFrequencyData(bandsData)
+        const peaksData = new Uint8Array(peaksBinCount)
+        peaksFrequencyData(peaksData)
+
+        visualize({
+          canvas: canvasRef.current,
+          bands: bands.map(i => bandsData[i]),
+          peaks: bands.map(i => peaksData[i]),
+        })
+
+        requestAnimationFrame(sendBands)
+      }
+
+      sendBands()
+    }, // eslint-disable-next-line
+    [controller]
   )
 
   return (
-    <Canvas ref={ spectrum } />
+    <Canvas ref={ canvasRef } />
   )
 }
---
+++ src/App/functions/visualize.js
@@ -0,0 +1,53 @@
+export default ({ canvas, bands, peaks }) => {
+  const ctx = canvas.getContext(`2d`)
+  const { width, height } = canvas
+  const wx = width / 79
+  const hx = height / 19
+
+  const clear = () => {
+    ctx.fillStyle = `hsl(0, 0%, 0%)`
+    ctx.fillRect(0, 0, width, height);
+
+    [...Array(19).keys()].forEach((y) => {
+      [...Array(79).keys()].forEach((x) => {
+        if (x % 2 === 0 && y % 2 === 0) {
+          ctx.fillStyle = x === 0
+            ? y % 4 === 0
+              ? `hsl(216, 60%, 81%)`
+              : `hsl(205, 100%, 71%)`
+            : y === 18
+              ? x % 4 === 0
+                ? `hsl(216, 60%, 81%)`
+                : `hsl(205, 100%, 71%)`
+              : `hsl(240, 41%, 16%)`
+
+          ctx.fillRect(x * wx, y * hx, wx, hx)
+        }
+      })
+    })
+  }
+
+  clear()
+
+  const gradient = ctx.createLinearGradient(0, 0, 0, 16 * hx)
+
+  gradient.addColorStop(0, `hsl(0, 100%, 50%)`)
+  gradient.addColorStop(.25, `hsl(30, 100%, 50%)`)
+  gradient.addColorStop(.5, `hsl(60, 100%, 50%)`)
+  gradient.addColorStop(.75, `hsl(90, 100%, 50%)`)
+  gradient.addColorStop(1, `hsl(120, 100%, 50%)`)
+
+  for (let i = 0; i < bands.length; i++) {
+    const band = Math.floor(16 / 255 * bands[i])
+    const peak = Math.max(Math.floor(16 / 255 * peaks[i]), band)
+    const x = i === 0
+      ? 2 * wx
+      : (((i - 1) * 4) + 6) * wx
+
+    ctx.fillStyle = gradient
+    ctx.fillRect(x, height - band * hx, 3 * wx, band * hx - 3)
+
+    ctx.fillStyle = `hsl(0, 0%, 90%)`
+    ctx.fillRect(x, height - 5 - peak * hx, 3 * wx, hx)
+  }
+}
